cf9612780447a146079ddb6e9804f35cacb3926d
juthaaa <45228816+juthaaa@users.noreply.github.com>
2020-11-22 12:25:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -98,7 +98,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => $text]]
+            'messages' => [['type' => 'text', 'text' => $text2]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
